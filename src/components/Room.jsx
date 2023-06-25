7965cf9aa9bab3256ef971f2c22840ed28c4f684
vishalspatil <vishalspolicepatil55@gmail.com>
2023-06-26 01:02:37 +0530

    leave room

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -1,14 +1,27 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const Room = () => {
     const { code } = useParams();
+    const navigate = useNavigate();
 
     const [data, setData] = useState({
         votes_to_skip: 2,
         guest_can_pause: false,
         is_host: false
     });
+
+    const handleLeaveRoom = () => {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }
+        }
+
+        fetch('/api/leave-room', requestOptions)
+        .then((_response) => {
+            navigate('/');
+        })
+    };
 
     const getData = () => {
         fetch('/api/get-room?code=' + code)
@@ -27,10 +40,17 @@ const Room = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Room</h1>
-            <h2>{data.votes_to_skip}</h2>
-            <h2>{data.is_host.toString()}</h2>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl">Code: {code}</h1>
+            <p className="text-2xl">Votes: {data.votes_to_skip}</p>
+            <p className="text-2xl">Guest Can Pause: {data.guest_can_pause ? 'Yes' : 'No'}</p>
+            <p className="text-2xl">Host: {data.is_host ? 'Yes' : 'No'}</p>
+            <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mt-4 rounded"
+                onClick={handleLeaveRoom}
+            >
+                Leave Room
+            </button>
         </div>
     )
 }
